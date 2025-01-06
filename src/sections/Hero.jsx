d17e7bda694986965710e7d3ecbf98fe7ef97891
Nelson25805 <91099339+Nelson25805@users.noreply.github.com>
2025-01-06 18:59:55 -0500

    basic project file, with navbar, and starting index page

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,14 @@
+const Hero = () => {
+    return (
+        <section className="min-h-screen w-full flex-col relative">
+            <div className="w-full mx-auto flex-col sm:mt-36 mt-20 c-space gap-3">
+                <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
+                    Hello, I am Nelson
+                    <span className="waving-hand">👋🏻</span></p>
+            </div>
+            Hero
+        </section>
+    )
+}
+
+export default Hero
