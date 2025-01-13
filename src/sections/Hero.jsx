4f1c43532e244078a77c42f934520c9906450a62
Nelson25805 <91099339+Nelson25805@users.noreply.github.com>
2025-01-13 01:40:08 -0500

    project updated to show 3d models, and shows contact button

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -7,11 +7,16 @@ import { useMediaQuery } from "react-responsive";
 import { calculateSizes } from "../constants/index.js";
 import Target from "../components/Target.jsx";
 import ReactLogo from "../components/ReactLogo.jsx";
+import Cube from "../components/Cube.jsx";
+import Rings from "../components/Rings.jsx";
+import HeroCamera from "../components/HeroCamera.jsx";
+import Button from "../components/Button.jsx";
+//import PikachuGameboy from "../components/PikachuGameboy.jsx";
 
 const Hero = () => {
-    const isSmall = useMediaQuery({maxWidth: 440});
-    const isMobile = useMediaQuery({maxWidth: 768});
-    const isTablet = useMediaQuery({minxWidth: 768, maxWidth: 1024});
+    const isSmall = useMediaQuery({ maxWidth: 440 });
+    const isMobile = useMediaQuery({ maxWidth: 768 });
+    const isTablet = useMediaQuery({ minxWidth: 768, maxWidth: 1024 });
 
     const sizes = calculateSizes(isSmall, isMobile, isTablet);
 
@@ -26,20 +31,30 @@ const Hero = () => {
             <div className="w-full h-full absolute inset-0">
                 {/* <Leva /> */}
                 <Canvas className="w-full h-full">
-                    <Suspense fallback={<CanvasLoader/>}>
+                    <Suspense fallback={<CanvasLoader />}>
                         <PerspectiveCamera makeDefault position={[0, 0, 20]} />
-                        <HackerRoom
-                            position={sizes.deskPosition}
-                            rotation={[0, -Math.PI, 0]}
-                            scale={sizes.deskScale} />
+                        <HeroCamera isMobile={isMobile}>
+                            <HackerRoom
+                                position={sizes.deskPosition}
+                                rotation={[0, -Math.PI, 0]}
+                                scale={sizes.deskScale} />
+                        </HeroCamera>
                         <group>
-                            <Target position={sizes.targetPosition}/>
-                            <ReactLogo position={sizes.reactLogoPosition}/>
+                            <Target position={sizes.targetPosition} />
+                            {/* <PikachuGameboy position={sizes.cubePosition} scale={1/4}/> */}
+                            <ReactLogo position={sizes.reactLogoPosition} />
+                            <Cube position={sizes.cubePosition} />
+                            <Rings position={sizes.ringPosition} />
                         </group>
                         <ambientLight intensity={1} />
                         <directionalLight position={[10, 10, 10]} intensity={0.5} />
                     </Suspense>
                 </Canvas>
+            </div>
+            <div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
+                <a href="#contact" className="w-fit">
+                    <Button name="Lets work together" isBeam containerClass="sm:w-fit w-full sm:min-w-96" />
+                </a>
             </div>
         </section>
     )
