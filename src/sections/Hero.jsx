e2931f92f60a3c74861a4eb97fcb78bff349e879
Nelson25805 <91099339+Nelson25805@users.noreply.github.com>
2025-01-15 02:27:01 -0500

    added navbar navigation to sections of page

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = () => {
     const sizes = calculateSizes(isSmall, isMobile, isTablet);
 
     return (
-        <section className="min-h-screen w-full flex-col relative">
+        <section className="min-h-screen w-full flex-col relative" id="home">
             <div className="w-full mx-auto flex-col sm:mt-36 mt-20 c-space gap-3">
                 <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
                     Hello, I am Nelson
