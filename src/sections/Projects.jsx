4bb365494c809046f946b6ae621bc62721a8255e
Nelson <Nelson25805@hotmail.com>
2025-05-15 01:26:58 -0400

    fixed bug where subdesc wasnt showing, and link to project wasnt working

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -40,7 +40,7 @@ const Projects = () => {
                     <div className="flex flex-col gap-5 text-white-600 my-5">
                         <p className="text-white text-2xl font-semibold animatedText">{currentProject.title}</p>
                         <p className="animatedText">{currentProject.desc}</p>
-                        <p className="animatedText">{currentProject.subdescdesc}</p>
+                        <p className="animatedText">{currentProject.subdesc}</p>
                     </div>
                     <div className="flex items-center justify-between flex-wrap gap-5">
                         <div className="flex items-center gap-3">
